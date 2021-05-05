4317705c4425560825f5d6321659859f395a8ce8
Santiago Lecuona <santiago.elias.lecuona@gmail.com>
2021-05-05 19:06:44 -0300

    refactor: CrudField class

--- crud.js
+++ crud.js
@@ -1,86 +1,17 @@
 const CrudJS = {};
 
-function createElement(e) {
+CrudJS.CreateElement = function(e) {
     return $(document.createElement(e));
-}
-
-function createFieldInput(f) {
-    const input = createElement('input');
-    input.addClass('form-control');
-    input.attr('type', f.type);
-    input.attr('id', f.id);
-
-    if(f.placeholder) input.attr('placeholder', f.placeholder);
-
-    f.container.append(input);
-}
-
-function createFieldTextarea(f) {
-    const textarea = createElement('textarea');
-    textarea.addClass('form-control');
-    textarea.attr('id', f.id);
-
-    if(f.rows) textarea.attr('rows', f.rows);
-
-    if(f.placeholder) textarea.attr('placeholder', f.placeholder);
-
-    f.container.append(textarea);
-}
-
-function createFieldContainer(f) {
-    const container = createElement('div');
-    container.addClass('mb-3');
-
-    const label = createElement('label');
-    label.addClass('form-label');
-    label.attr('for', f.id);
-    label.html(f.label);
-
-    container.append(label);
-    return container;
-}
-
-function createField(cjs, f) {
-    console.log("Creating field " + f.name);
-
-    f.id = 'cjs_' + f.name;
-    f.container = createFieldContainer(f);
-
-    cjs.form.append(f.container);
-
-    if(f.type == "text" || f.type == "email") {
-        createFieldInput(f);
-        return;
-    }
-    if(f.type == "textarea") {
-        createFieldTextarea(f);
-        return;
-    }
 }
 
 CrudJS.BuildForm = function(id, options) {
     console.log("Building form...");
-
-    // const form = createElement('form');
-
-    // const cjs = {
-    //     Id: id,
-    //     Options: options,
-    //     Container: $('#'+id),
-    //     Form: form
-    // }    
-
-    // options.fields.forEach(f => createField(cjs, f));
-
-    // cjs.Container.append(cjs.Form);
-    // return cjs;
 
     const form = new CrudForm(id, options);
     form.build();
     form.render();
     return form;
 }
-
 
 class CrudForm {
     constructor(id, options) {
@@ -92,12 +23,88 @@ class CrudForm {
     }
   
     build() {
-        this.form = createElement('form');
+        this.form = CrudJS.CreateElement('form');
         this.container = $('#'+this.id);
-        this.options.fields.forEach(f => createField(this, f));
+
+        this.options.fields.forEach(f => 
+            this.fields.push(new CrudFormField(f)));
+
+        this.fields.forEach(f => 
+            f.build());
     };
   
     render() {
+        this.fields.forEach(f => 
+            f.render());
+        this.fields.forEach(f => 
+            this.form.append(f.container));
         this.container.append(this.form);
     };
+}
+
+class CrudFormField {
+    constructor(options) {
+        this.options = options;
+        this.id = 'cjs_field_' + this.options.name;
+        this.container = null;
+        this.control = null;
+        this.label = null;
+    }
+  
+    build() {
+        this.buildContainer();
+        this.control = this.buildControl();
+    };
+
+    buildContainer() {
+        this.container = CrudJS.CreateElement('div');
+        this.container.addClass('mb-3');
+    
+        this.label = CrudJS.CreateElement('label');
+        this.label.addClass('form-label');
+        this.label.attr('for', this.id);
+        this.label.html(this.options.label);
+    };
+
+    buildControl() {
+        const type = this.options.type;
+        if(type == "text" || type == "email") {
+            return this.buildInput();
+        }
+        if(type == "textarea") {
+            return this.buildTextarea();
+        }
+        return null;
+    }
+
+    buildInput() {
+        const input = CrudJS.CreateElement('input');
+        input.addClass('form-control');
+        input.attr('type', this.options.type);
+        input.attr('id', this.id);
+    
+        if(this.options.placeholder) 
+            input.attr('placeholder', this.options.placeholder);
+        
+        return input;
+    }
+
+    buildTextarea() {
+        const textarea = CrudJS.CreateElement('textarea');
+        textarea.addClass('form-control');
+        textarea.attr('id', this.id);
+    
+        if(this.options.rows) 
+            textarea.attr('rows', this.options.rows);
+    
+        if(this.options.placeholder) 
+            textarea.attr('placeholder', this.options.placeholder);
+    
+        return textarea;
+    }
+
+    render() {
+        this.container.append(this.label);
+        this.container.append(this.control);
+    }
 }
